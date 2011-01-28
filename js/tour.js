dc98a2d0cb12b806b4a123ad26053a547dbe60ad
MadeMyDay <mh@siebennull.com>
2011-01-28 20:29:18 +0100

    commited correct tour.js

--- js/tour.js
+++ js/tour.js
@@ -5,6 +5,7 @@ $(function() {
 				tooltipWidth: Standard width of your tooltips. Can be altered in every step
 						
 				Settings for each step of your presentation:
+				screenNumber: The image to be shown. Same order as in your index.html.
 				name: a unique identifier for your tooltip / screen / intro page. Serves as anchor name (default: step[number])
 				showAsIntro: If true, an overlay is generated. Used for introducing a set of tooltips.
 				posX: Position of the tooltip on x-axis (not needed on intro pages or if text is empty)
@@ -38,7 +39,7 @@ $(function() {
 						"tooltipWidth" : 450,
 						"bgcolor"	: "black",
 						"position"	: "TL",
-						"text"		: "<b>Menüalternativen</b><br><br>Hallo Henning, hier ein paar Alternativen für die Menüdarstellung. Kommentare sind denke ich nicht nötig, schreib mir einfach welche Alternative Dir gefällt (kannst den Link kopieren aus der Adresszeile). Die Serifen könnten doch ganz gut funktionieren denke ich.</small>"
+						"text"		: "<b>Welcome to Screen Presenter!</b><br>V1 | 26.01.2011<br><br>Screen Presenter is a tool for showing your layouts to customers. Neither PDFs nor mail attachments are a appropriate way of showing something designed for a browser.<br><br><small>For navigating through the tour use the buttons on the bottom left.</small>"
 					},
 					{
 						"screenNumber" 		: 1,
@@ -52,114 +53,136 @@ $(function() {
 						"text"		: ""
 					},
 					{
-						"screenNumber" 		: 2,
-						"name"		: "screen2",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
+						"screenNumber" 		: 1,
 						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
+						"name"		: "intro",
+						"showAsIntro"	: false,
+						"posX"		: 5,
+						"posY"		: 190,
+						"tooltipWidth" : 250,
+						"text"		: "Oh my god, tooltips. Nice. Explain what your thoughts were while designing this layout",
+						"position"	: "TL"
+					},
+					{
+						"screenNumber" 		: 1,
+						"name"		: "logo",
+						"bgcolor"	: "black",
+						"posX"		: 240,
+						"showAsIntro"	: false,
+						"posY"		: 100,
+						"tooltipWidth" : 250,
+						"text"		: "Outstanding logo. You can put your arrows where you like (left in this case)",
+						"position"	: "L"
+					},
+					{
+						"screenNumber" 		: 1,
+						"bgcolor"	: "black",
+						"name"		: "menu",
+						"showAsIntro"	: false,
+						"posX"		: 800,
+						"posY"		: 90,
+						"tooltipWidth" : 500,
+						"text"		: "Clean and usable Navigation. Got it? Tooltip's width is <b>adjustable</b>",
+						"position"	: "T"
+					},
+					{
+						"screenNumber" 		: 1,
+						"bgcolor"	: "black",
+						"name"		: "scroll",
+						"showAsIntro"	: false,
+						"posX"		: 550,
+						"posY"		: 1170,
+						"tooltipWidth" : 500,
+						"text"		: "It's 2011, <b>animated scrolling</b> if tooltip is out of viewport. Next.",
+						"position"	: "B"
+					},
+					{
+						"screenNumber" 		: 1,
+						"bgcolor"	: "white",
+						"name"		: "footer",
+						"showAsIntro"	: false,
+						"posX"		: 550,
+						"posY"		: 1320,
+						"tooltipWidth" : 500,
+						"text"		: "Dark in here... so tooltip's <b>background</b> is adjustable, too",
+						"position"	: "T"
+					},
+					{
+						"screenNumber" 		: 1,
+						"bgcolor"	: "black",
+						"name"		: "address",
+						"showAsIntro"	: false,
+						"posX"		: 300,
+						"posY"		: 8,
+						"tooltipWidth" : 700,
+						"text"		: "BTW... did you see that the URL is changing? Yeah, <b>bookmarking, deep linking and browser history navigating</b>. Of course. Try it.",
+						"position"	: "T"
+					},
+{
+						"screenNumber" 		: 1,
+						"bgcolor"	: "black",
+						"name"		: "intro2",
+						"showAsIntro"	: true,
+						"posX"		: 300,
+						"posY"		: 5,
+						"tooltipWidth" : 700,
+						"text"		: "Kinda boring showing only <b>one</b> layout. Of course you can include multiple screens. Let's take a look...",
+						"position"	: "T"
+					},
+					{
+						"screenNumber" 		: 2,
+						"bgcolor"	: "black",
+						"showAsIntro"	: false,
+						"name"		: "layout2",
+						"posX"		: 630,
+						"posY"		: 270,
+						"tooltipWidth" : 150,
+						"text"		: "Here we are, another layout. You can switch between them as you like",
+						"position"	: "B"
+					},
+					{
+						"screenNumber" 		: 1,
+						"bgcolor"	: "black",
+						"name"		: "backtolayout1",
+						"showAsIntro"	: false,
+						"posX"		: 250,
+						"posY"		: 250,
+						"tooltipWidth" : 240,
+						"text"		: "Yes, nice fade effect.",
+						"position"	: "R"
 					},
 					{
 						"screenNumber" 		: 3,
-						"name"		: "screen3",
+						"name"		: "blackone",
+						"posX"		: 100,
+						"posY"		: 1170,
 						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
+						"tooltipWidth" : 300,
+						"bgcolor"	: "white",
+						"text"		: "Combined screen change and scroll to new tooltip. Works.",
+						"position"	: "B"
 					},
 					{
-						"screenNumber" 		: 4,
-						"name"		: "screen4",
+						"screenNumber" 		: 3,
+						"name"		: "blackone2",
+						"posX"		: 30,
+						"posY"		: 390,
 						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
+						"tooltipWidth" : 250,
+						"bgcolor"	: "white",
+						"text"		: "Did you click the feedback button? Remember, it is <b>only a layout</b>, so there is nothing to click",
+						"position"	: "L"
 					},
 					{
-						"screenNumber" 		: 5,
-						"name"		: "screen5",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
-					},
-					{
-						"screenNumber" 		: 6,
-						"name"		: "screen6",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
-					},
-					{
-						"screenNumber" 		: 7,
-						"name"		: "screen7",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
-					},
-					{
-						"screenNumber" 		: 8,
-						"name"		: "screen8",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
-					},
-					{
-						"screenNumber" 		: 9,
-						"name"		: "screen9",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
-					},
-					{
-						"screenNumber" 		: 10,
-						"name"		: "screen10",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
-					},
-					{
-						"screenNumber" 		: 11,
-						"name"		: "screen11",
-						"showAsIntro"	: false,
-						"posX"		: 200,
-						"posY"		: 200,
-						"tooltipWidth" : 450,
-						"bgcolor"	: "black",
-						"position"	: "TL",
-						"text"		: ""
+						"screenNumber" 		: 1,
+						"posX"		: 400,
+						"name"		: "ende",
+						"posY"		: 990,
+						"showAsIntro"	: true,
+						"tooltipWidth" : 630,
+						"bgcolor"	: "white",
+						"text"		: "Thank you. Over and out.",
+						"position"	: "B"
 					}
 				],
 				previousStep = false,
